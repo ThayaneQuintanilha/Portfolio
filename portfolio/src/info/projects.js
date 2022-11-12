71da683329c3bd18b5d778891dad809ef9fd202f
root <root@DESKTOP-AE7812C.localdomain>
2022-11-12 14:35:19 -0300

    feat: adiciona dados e projetos

---
+++ portfolio/src/info/projects.js
@@ -0,0 +1,17 @@
+const projects = [
+  {
+    image: '../assets/pikachu.jpg',
+    title: 'Projeto 1',
+    description: 'is simply dummy text of the trys standard dummy text ever since the 1500s',
+  },
+  {
+    image: '../assets/pikachu.jpg',
+    title: 'Projeto 2',
+    description: 'is simply dummy text of the trys standard dummy text ever since the 1500s',
+  },
+  {
+    image: '../assets/pikachu.jpg',
+    title: 'Projeto 3',
+    description: 'is simply dummy text of the trys standard dummy text ever since the 1500s',
+  },
+];
