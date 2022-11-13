a9f57fad9fadbc195af4023deeb58890d53c10a2
root <root@DESKTOP-AE7812C.localdomain>
2022-11-13 17:30:07 -0300

    fstyle: aestilo do componente AboutMe

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -6,15 +6,20 @@ import personal from '../info/personal';
 function AboutMe() {
   const { me: { description, description2 } } = personal;
   return (
-    <section>
-      <div>
+    <section className="about-section">
+      <div className="about-img">
         <img src={number1} alt="numero 1" />
         <h2>sobre mim</h2>
       </div>
-      <div>
+      <div className="about-description">
         <p>{description}</p>
-        <p></p>
+        <div className="about-rectangle"></div>
         <p>{description2}</p>
+      </div>
+      <div className="about-rectangle-radius">
+        <p>~</p>
+        <p>~</p>
+        <p>~</p>
       </div>
     </section>
   );
