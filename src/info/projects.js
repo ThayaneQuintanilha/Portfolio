e5efcf2a5419891f0d62a04affbdeca206865be2
root <root@DESKTOP-AE7812C.localdomain>
2022-11-13 18:57:35 -0300

    style: adiciona estilo ao componente projects + implementações

--- src/info/projects.js
+++ src/info/projects.js
@@ -5,16 +5,19 @@ const projects = [
     image: pikachu,
     title: 'Projeto 1',
     description: 'is simply dummy text of the trys standard dummy text ever since the 1500s',
+    link: 'https://github.com/ThayaneQuintanilha',
   },
   {
     image: pikachu,
     title: 'Projeto 2',
     description: 'is simply dummy text of the trys standard dummy text ever since the 1500s',
+    link: 'https://github.com/ThayaneQuintanilha',
   },
   {
     image: pikachu,
     title: 'Projeto 3',
     description: 'is simply dummy text of the trys standard dummy text ever since the 1500s',
+    link: 'https://github.com/ThayaneQuintanilha',
   },
 ];
 
