552f0437ea430f92359061d074081c36a38071b4
root <root@DESKTOP-AE7812C.localdomain>
2022-11-14 17:31:59 -0300

    style: css do componente Contact

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -11,18 +11,18 @@ function Contact() {
     <section className="contact-section">
       <div className="contact-rectangle"></div>
       <div className="contact-img">
-        <img src={number4} alt="numero 4" />
+        <img className="contact-tag-img" src={number4} alt="numero 4" />
           <h2>contato</h2>
           <h3>Estamos a um click de</h3>
-        <div>
+        <div className="contact-star">
           <img src={star} alt="estrela" />
           <h3>distância</h3>
           <img src={star} alt="estrela" />
         </div>
-        <div>
-          <a target="_blank" href="mailto:thayaniquintanilha2@gmail.com"><img src={Gmail} alt="estrela" /></a>
-          <a target="_blank" href="https://github.com/ThayaneQuintanilha"><img src={Github} alt="estrela" /></a>
-          <a target="_blank" href="https://www.linkedin.com/in/thayanequintanilha"><img src={Linkedin} alt="estrela" /></a>
+        <div className="contact-links">
+          <a target="_blank" href="mailto:thayaniquintanilha2@gmail.com"><img src={Gmail} alt="gmail" /></a>
+          <a target="_blank" href="https://github.com/ThayaneQuintanilha"><img src={Github} alt="github" /></a>
+          <a target="_blank" href="https://www.linkedin.com/in/thayanequintanilha"><img src={Linkedin} alt="linkedin" /></a>
         </div>
       </div>
       <div className="contact-rectangle"></div>
